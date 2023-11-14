e584c31e83306f0f058712fc6e084711ebcaedcd
Géczi Kornél <hungarokornel@gmail.com>
2023-11-14 12:14:32 +0100

    Add project files.

---
+++ hotellift/Lift.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotellift
+{
+    internal class Lift
+    {
+        DateTime usage;
+        int cardNumber, startNumber, endNumber;
+
+        public Lift(string row)
+        {
+            string[] data = row.Split(' ');
+            string[] date = data[0].Split('.');
+            this.usage = new DateTime(int.Parse(date[0]), int.Parse(date[1]), int.Parse(date[2]));
+            this.cardNumber = int.Parse(data[1]);
+            this.startNumber = int.Parse(data[2]);
+            this.endNumber = int.Parse(data[3]);
+        }
+
+        public DateTime Usage { get => usage; }
+        public int CardNumber { get => cardNumber; }
+        public int StartNumber { get => startNumber; }
+        public int EndNumber { get => endNumber; }
+    }
+}
---
+++ hotellift/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace hotellift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Lift> lifts = new List<Lift>();
+
+            StreamReader sr = new StreamReader("lift.txt");
+
+            while (!sr.EndOfStream)
+            {
+                Lift lift = new Lift(sr.ReadLine());
+                lifts.Add(lift);
+            }
+
+            sr.Close();
+
+            Console.WriteLine($"3. feladat: Összes lifthasználat: {lifts.Count()}");
+            Console.WriteLine($"4. feladat: Időszak: {lifts.Min(x => x.Usage).Date} - {lifts.Max(x => x.Usage).Date}");
+            Console.WriteLine($"5. feladat: Célszint max: {lifts.Max(x => x.EndNumber)}");
+            Console.WriteLine("6. feladat:");
+            int cardNumber;
+            int endNumber;
+            try
+            {
+                Console.Write("\tKártya száma: ");
+                cardNumber = int.Parse(Console.ReadLine());
+                Console.Write("\tCélszint száma: ");
+                endNumber = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                cardNumber = 5;
+                endNumber = 5;
+            }
+
+            Console.WriteLine($"A(z) {lifts.Where(x => x.CardNumber == cardNumber).First().CardNumber}. kártyával nem utaztak a {lifts.Where(x => x.EndNumber == endNumber).First().EndNumber}. emeletre!");
+
+            Console.WriteLine("8. feladat: Statisztika");
+            lifts.GroupBy(x => x.Usage).ToList().ForEach(x => Console.WriteLine($"{x.Key} - {x.Count()}x"));
+
+        }
+    }
+}
